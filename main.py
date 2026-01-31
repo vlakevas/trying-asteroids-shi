cd4ab84624b957c8d15249f5758728a1e02115a7
vlakevas <gearspeed64@gmail.com>
2026-01-31 02:20:20 +0200

    INITIAL COMMIT

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while(1):
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
