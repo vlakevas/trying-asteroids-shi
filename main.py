e5a7e6ea4c00ed552d0051d0dca414ce05b3d549
vlakevas <gearspeed64@gmail.com>
2026-02-02 22:56:28 +0200

    CH3 COMMIT

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2 , SCREEN_HEIGHT / 2)
+    
     while(1):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill("black")
+        
+        for thing in drawable:
+            thing.draw(screen)
+        
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
+        
+    
         
 
 if __name__ == "__main__":
