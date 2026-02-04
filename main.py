a59d22fbd21aa07f58f08640d3b3698effde76ee
vlakevas <gearspeed64@gmail.com>
2026-02-04 23:13:23 +0200

    CH4-added asteroids model

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
     player = Player(SCREEN_WIDTH/2 , SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     
     while(1):
         log_state()
